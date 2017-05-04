cf9ea1bf54b81e1879e55a4b0f8e43cb084aaeb7
Kristina <kmatuleviciute@tssg.org>
2017-05-04 14:09:56 +0100

    edit, profile, backend

--- client/components/Row.js
+++ client/components/Row.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Nav, Button } from 'react-bootstrap'
 import EditTable from './EditTable'
+import Profile from './Profile'
 
 export default class Row extends React.Component {
   constructor(props) {
@@ -12,18 +13,19 @@ export default class Row extends React.Component {
     this.props.onDelete( this.props.item );
     return false
   }
-  viewAdmins(props) {
-    this.props.onViewAdmins( this.props.item.id );
+  viewProfile(props) {
+    this.props.onViewprofile( this.props.item._id );
     return false
   }
   render (){
     return (
       <tr>
-      <td>{this.props.item.user.name}</td>
-      <td>{this.props.item.user.surname}</td>
-      <td>{this.props.item.user.email}</td>
-      <td>{this.props.item.user.phone}</td>
-      <td>edit</td>
+      <td><Button bsStyle="info" style={{"marginTop": "0px"}} onClick={this.viewProfile.bind(this)}><span>{this.props.item.name}</span></Button></td>
+      <td>{this.props.item.address}</td>
+      <td>{this.props.item.phone_number}</td>
+      <td>{this.props.item.email}</td>
+      <td>{this.props.item.age}</td>
+      <td><EditTable item={this.props.item}/></td>
       <td><Button bsStyle="danger" style={{"marginTop": "0px"}} onClick={this.handleRemove.bind(this)}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></Button></td>
       </tr>
     )
